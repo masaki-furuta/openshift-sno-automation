d1f946d335b65f042a3e09bd27aa5e260fddc5ee
Masaki Furuta <masaki@t8-plus-neon.local>
2025-05-23 20:22:20 +0900

    Refactor: Add explicit cd and copy steps for contrib/, devel/, and secrets/
    - Ensured scripts are copied into target subdirectories (`contrib/`, `devel/`, `secrets/`) using `cd` for clarity
    - Added `cd -` to return to the original directory after each block
    - No functional logic changed, but improves readability and structure of file operations

--- create-openshift-sno-structure.sh
+++ create-openshift-sno-structure.sh
@@ -50,11 +50,19 @@ SNO_MAC_ADDR=$(grep -E '^ *mac_addr:' "$ROOT_DIR/ansible/inventory.yaml"|cut -d:
 SNO_DNS=$(grep      -E '^ *dns:'      "$ROOT_DIR/ansible/inventory.yaml"|cut -d: -f2-|sed -e 's/ *//g')
 SNO_GATEWAY=$(grep  -E '^ *gateway:'  "$ROOT_DIR/ansible/inventory.yaml"|cut -d: -f2-|sed -e 's/ *//g')
 
+cd secrets/
 cp pull-secret.txt id_rsa.pub "$ROOT_DIR/secrets/"
+cd -
 PULL_SECRET_CONTENT=$(cat "$ROOT_DIR/secrets/pull-secret.txt")
 SSH_KEY_CONTENT=$(cat "$ROOT_DIR/secrets/id_rsa.pub")
 
+cd contrib/
 cp install-openshift-bin_v3.sh  install-virtualbox-vnc.sh  oc-login_v4.sh  select-failed-pods-to-delete.sh  tp-fan-control.sh "$ROOT_DIR/contrib/"
+cd -
+
+cd devel
+cp new-version.sh "$ROOT_DIR/devel/"
+cd -
 
 echo "Delete $ROOT_DIR/deployment/* and dotfiles? (y/N)"
 read -r ans
